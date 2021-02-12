66aefe29374409dc7e982501789ca0403f410c2f
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2021-02-12 20:25:52 +0100

    Now using structopt

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,13 @@
 
-use clap::{App, Arg};
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
 
 fn main() {
-    let app = App::new("grrs")
-        .version("1.0")
-        .about("A mini grep in rust")
-        .author("Tiago Coutinho")
-        .arg(Arg::with_name("pattern")
-             .short("p")
-             .long("pattern")
-             .takes_value(true)
-             .required(true)
-             .help("pattern to match"))
-        .arg(Arg::with_name("path")
-             .required(true)
-             .long("path")
-             .takes_value(true)
-             .help("file to inspect"));
-    let matches = app.get_matches();
-
-    let pattern = matches.value_of("pattern").unwrap();
-    let path = matches.value_of("path").unwrap();
-
-    println!("pattern=\"{:}\" path=\"{:}\"", pattern, path);
+    let cli = Cli::from_args();
+    println!("pattern=\"{:}\" path={:#?}", cli.pattern, cli.path);
 }
