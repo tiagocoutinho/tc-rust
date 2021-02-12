9ce3226a79071076d9cb62fc4a79cabe3d9666a7
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2021-02-12 20:20:26 +0100

    Using clap

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
 
+use clap::{App, Arg};
 
 fn main() {
+    let app = App::new("grrs")
+        .version("1.0")
+        .about("A mini grep in rust")
+        .author("Tiago Coutinho")
+        .arg(Arg::with_name("pattern")
+             .short("p")
+             .long("pattern")
+             .takes_value(true)
+             .required(true)
+             .help("pattern to match"))
+        .arg(Arg::with_name("path")
+             .required(true)
+             .long("path")
+             .takes_value(true)
+             .help("file to inspect"));
+    let matches = app.get_matches();
 
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no path given");
+    let pattern = matches.value_of("pattern").unwrap();
+    let path = matches.value_of("path").unwrap();
 
     println!("pattern=\"{:}\" path=\"{:}\"", pattern, path);
 }
