6d5e08a4b5ccbb2ac739f989335338df343b54b6
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2021-02-13 19:11:00 +0100

    Replace unwrap with "?"

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,13 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> Result<(), std::io::Error> {
     let cli = Cli::from_args();
-    let content = std::fs::read_to_string(&cli.path).unwrap();
+    let content = std::fs::read_to_string(&cli.path)?;
     for line in content.lines() {
         if line.contains(&cli.pattern) {
             print!("{}", line);
         }
     }
+    Ok(())
 }
