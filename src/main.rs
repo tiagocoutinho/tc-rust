6751a7916963cd6f8a23b97152eac423ebab1c11
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2021-02-14 17:54:46 +0100

    Add logging

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-
+use log::{info};
 use structopt::StructOpt;
 use anyhow::{Context, Result};
 
@@ -8,10 +8,19 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
+//
+// RUST_LOG=tc_grrs=info cargo run --bin tc-grrs -- struct ./Cargo.toml
+
 fn main() -> Result<()> {
+    env_logger::init();
+    info!("parsing command line...");
     let cli = Cli::from_args();
+    info!("parsed command line");
+    info!("loading {:?}...", cli.path);
     let content = std::fs::read_to_string(&cli.path).
         with_context(|| format!("could not read file `{:?}`", cli.path))?;
+    info!("loaded {:?}", cli.path);
+    info!("searching for '{}'...", cli.pattern);
     for line in content.lines() {
         if line.contains(&cli.pattern) {
             print!("{}", line);
