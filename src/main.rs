7c0644f4f371a7cc1854ee0702f6c830105b6c65
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2021-02-14 21:45:40 +0100

    Split code

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,6 @@ struct Cli {
 }
 
 
-fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) -> Result<()> {
-    for line in content.lines() {
-        if line.contains(&pattern) {
-            writeln!(writer, "{}", line)?;
-        }
-    }
-    Ok(())
-}
-
 //
 // RUST_LOG=tc_grrs=info cargo run --bin tc-grrs -- struct ./Cargo.toml
 
@@ -32,16 +23,7 @@ fn main() -> Result<()> {
         with_context(|| format!("could not read file `{}`", args.path.display()))?;
     info!("loaded `{}`", args.path.display());
     info!("searching for `{}`...", args.pattern);
-    find_matches(&content, &args.pattern, &mut std::io::stdout()).
+    tc_grrs::find_matches(&content, &args.pattern, &mut std::io::stdout()).
         with_context(|| format!("could not print results!"))?;
-    Ok(())
-}
-
-
-#[test]
-fn find_a_match() -> Result<()> {
-    let mut result = Vec::new();
-    find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result)?;
-    assert_eq!(result, b"lorem ipsum\n");
     Ok(())
 }
