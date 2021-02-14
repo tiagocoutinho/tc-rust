b4694bde46aee86c34182ce68a8d1f13ef21bf97
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2021-02-14 21:40:04 +0100

    Add unit test

--- src/main.rs
+++ src/main.rs
@@ -9,23 +9,39 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
+
+fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) -> Result<()> {
+    for line in content.lines() {
+        if line.contains(&pattern) {
+            writeln!(writer, "{}", line)?;
+        }
+    }
+    Ok(())
+}
+
 //
 // RUST_LOG=tc_grrs=info cargo run --bin tc-grrs -- struct ./Cargo.toml
 
 fn main() -> Result<()> {
     env_logger::init();
     info!("parsing command line...");
-    let cli = Cli::from_args();
+    let args = Cli::from_args();
     info!("parsed command line");
-    info!("loading {:?}...", cli.path);
-    let content = std::fs::read_to_string(&cli.path).
-        with_context(|| format!("could not read file `{:?}`", cli.path))?;
-    info!("loaded {:?}", cli.path);
-    info!("searching for '{}'...", cli.pattern);
-    for line in content.lines() {
-        if line.contains(&cli.pattern) {
-            print!("{}", line);
-        }
-    }
+    info!("loading `{}`...", args.path.display());
+    let content = std::fs::read_to_string(&args.path).
+        with_context(|| format!("could not read file `{}`", args.path.display()))?;
+    info!("loaded `{}`", args.path.display());
+    info!("searching for `{}`...", args.pattern);
+    find_matches(&content, &args.pattern, &mut std::io::stdout()).
+        with_context(|| format!("could not print results!"))?;
+    Ok(())
+}
+
+
+#[test]
+fn find_a_match() -> Result<()> {
+    let mut result = Vec::new();
+    find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result)?;
+    assert_eq!(result, b"lorem ipsum\n");
     Ok(())
 }
