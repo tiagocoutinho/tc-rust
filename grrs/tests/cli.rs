29517b0a158de17fe0131229519211a765d6f5a7
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2021-02-15 19:47:42 +0100

    grrs: Add test with file

--- grrs/tests/cli.rs
+++ grrs/tests/cli.rs
@@ -1,6 +1,7 @@
 use assert_cmd::prelude::*; // Add methods on commands
 use predicates::prelude::*; // Used for writing assertions
 use std::process::Command;
+use std::io::Write;
 
 #[test]
 fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
@@ -9,5 +10,17 @@ fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("No such file or directory"));
+    Ok(())
+}
+
+#[test]
+fn find_in_file() -> Result<(), Box<dyn std::error::Error>> {
+    let mut file = tempfile::NamedTempFile::new()?;
+    writeln!(file, "Lets test\na brand new\nfile\nWith brand\nnew content")?;
+    let mut cmd = Command::cargo_bin("grrs")?;
+    cmd.arg("brand").arg(file.path());
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("a brand new\nWith brand\n"));
     Ok(())
 }
