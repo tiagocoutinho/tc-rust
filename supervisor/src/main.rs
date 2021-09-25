c08ba2fe19a482d87387660ba127e6f6f68dd21a
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2021-09-25 16:36:36 +0200

    First prototype

---
+++ supervisor/src/main.rs
@@ -0,0 +1,20 @@
+use async_process::Command;
+
+#[async_std::main]
+async fn main() {
+    let out = Command::new("python")
+        .arg("-m")
+        .arg("http.server")
+        .output()
+        .await;
+    println!("{:?}", out);
+}
+
+/*
+use std::process::Command;
+
+fn main() {
+    let out = Command::new("python").arg("-m").arg("http.server").output();
+    println!("{:?}", out);
+}
+*/
