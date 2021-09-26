c74c9873e07dad985e29313a54367801c9960e73
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2021-09-26 11:11:06 +0200

    Monitor output (line buffer mode)

---
+++ supervisor/go.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo -n "Preparing........ "
+sleep $1
+echo "[DONE]"
+echo -n "Running system... "
+sleep $1
+echo "[DONE]"
+sleep 0.1
+exit 1
--- supervisor/src/main.rs
+++ supervisor/src/main.rs
@@ -1,20 +1,23 @@
-use async_std::process::Command;
+use async_std::{
+    io::BufReader,
+    prelude::*,
+    process::{Command, Stdio},
+};
+
+fn cmd(sleep: u32) -> Command {
+    let mut cmd = Command::new("./go.sh");
+    cmd.arg(sleep.to_string()).stdout(Stdio::piped());
+    cmd
+}
 
 #[async_std::main]
 async fn main() {
-    let out = Command::new("python")
-        .arg("-m")
-        .arg("http.server")
-        .output()
-        .await;
-    println!("{:?}", out);
+    let proc = cmd(1).spawn().expect("Error creating process");
+    let stdout = proc.stdout.expect("No stdout available!");
+    let reader = BufReader::new(stdout);
+    let mut lines = reader.lines();
+    while let Some(line) = lines.next().await {
+        println!("line: {:?}", line);
+    }
+    println!("---END---");
 }
-
-/*
-use std::process::Command;
-
-fn main() {
-    let out = Command::new("python").arg("-m").arg("http.server").output();
-    println!("{:?}", out);
-}
-*/
