8857e4dd17e378e4272c934d12b72305d9e3982a
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2021-09-26 19:06:37 +0200

    Use modules

--- supervisor/src/main.rs
+++ supervisor/src/main.rs
@@ -1,14 +1,14 @@
-use supervisor::{line_output, non_buffered_output};
+use supervisor::output;
 
 #[async_std::main]
 async fn main() {
-    if let Err(error) = non_buffered_output("./go.sh", &["1", "0"], false).await {
+    if let Err(error) = output::non_buffered_output("./go.sh", &["1", "0"], false).await {
         eprintln!("> Error: {}", error);
     }
-    if let Err(error) = non_buffered_output("./go.sh", &[".1", "3"], true).await {
+    if let Err(error) = output::non_buffered_output("./go.sh", &[".1", "3"], true).await {
         eprintln!("> Error: {}", error);
     }
-    if let Err(error) = line_output("./go.sh", &["1.1", "5"], false).await {
+    if let Err(error) = output::line_output("./go.sh", &["1.1", "5"], false).await {
         eprintln!("> Error: {}", error);
     }
 }
